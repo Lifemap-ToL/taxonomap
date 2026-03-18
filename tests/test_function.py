397b63e5a76faa25733c567f7b5de2d65ca45045
briou2 <baptisteriou@yahoo.fr>
2026-03-18 10:23:01 +0100

    Convert test functions into test class

--- tests/test_function.py
+++ tests/test_function.py
@@ -3,53 +3,45 @@ import pytest
 
 from taxonomap.function import taxid_to_latin_name
 
+class Test_taxid_to_latin_name : 
 
 
-
-def test_invalid_type_string():
-    """Test with string instead ofa integer"""
-    with pytest.raises(ValueError):
-        taxid_to_latin_name("965")
-
+    def test_invalid_type_string(self):
+        """Test with string instead ofa integer"""
+        with pytest.raises(ValueError):
+            taxid_to_latin_name("965")
 
 
-def test_invalid_type_float():
-    """Test with float instead of a integer"""
-    with pytest.raises(ValueError):
-        taxid_to_latin_name(3.14)
+    def test_invalid_type_float(self):
+        """Test with float instead of a integer"""
+        with pytest.raises(ValueError):
+            taxid_to_latin_name(3.14)
 
 
-
-def test_negative_taxid():
-    """Test with a negative integer"""
-    with pytest.raises(ValueError) :
-        taxid_to_latin_name(-5)
-
+    def test_negative_taxid(self):
+        """Test with a negative integer"""
+        with pytest.raises(ValueError) :
+            taxid_to_latin_name(-5)
 
 
-def test_zero_taxid():
-    """Test with taxid = 0"""
-    with pytest.raises(ValueError):
+    def test_zero_taxid(self):
+        """Test with taxid = 0"""
+        with pytest.raises(ValueError):
 
-        taxid_to_latin_name(0)
-
-
+            taxid_to_latin_name(0)
 
 
-
-def test_valid_taxid():
-    """Test with a correct taxid"""
-    result = taxid_to_latin_name(965)
-    assert result is not str
-    assert len(result) > 0
-
+    def test_valid_taxid(self):
+        """Test with a correct taxid"""
+        result = taxid_to_latin_name(965)
+        assert result is not str
+        assert len(result) > 0
 
 
-
-def test_invalid_taxid():
-    """Test with a non-existing taxid"""
-    with pytest.raises(ValueError):
-        taxid_to_latin_name(999999999999999999)
+    def test_invalid_taxid(self):
+        """Test with a non-existing taxid"""
+        with pytest.raises(ValueError):
+            taxid_to_latin_name(999999999999999999)
 
 
 
