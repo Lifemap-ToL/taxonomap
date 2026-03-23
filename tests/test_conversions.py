24d94665a6918b1c0bb7e329b69fc1ceb3d3a19c
briou2 <baptisteriou@yahoo.fr>
2026-03-23 11:11:54 +0100

    Split test_function.py into 3 modules: test_conversions.py, test_pylogeny.py test_validation.py

---
+++ tests/test_conversions.py
@@ -0,0 +1,36 @@
+import pytest
+
+from taxonomap.function import taxid_to_latin_name, get_all_ascendant,valid_taxid,convert_taxid, get_MRCA_taxid
+
+class Test_taxid_to_latin_name : 
+
+    def test_valid_type_string(self):
+        """Test with string instead ofa integer"""
+        result = taxid_to_latin_name("965")
+        assert result == "Oceanospirillum"
+
+
+    def test_invalid_type_float(self):
+        """Test with float instead of a integer"""
+        with pytest.raises(ValueError):
+            taxid_to_latin_name(3.14)
+
+
+
+    def test_negative_taxid(self):
+        """Test with a negative integer"""
+        with pytest.raises(ValueError) :
+            taxid_to_latin_name(-5)
+
+    def test_valid_taxid(self):
+        """Test with a correct taxid"""
+        result = taxid_to_latin_name(965)
+        assert result is not str
+        assert len(result) > 0
+        assert result == "Oceanospirillum"
+
+
+    def test_invalid_taxid(self):
+        """Test with a non-existing taxid"""
+        result = taxid_to_latin_name(9999999999999)
+        assert result == None
---
+++ tests/test_validation.py
@@ -0,0 +1,76 @@
+import pytest
+
+from taxonomap.function import taxid_to_latin_name, get_all_ascendant,valid_taxid,convert_taxid, get_MRCA_taxid
+
+
+
+class Test_valid_taxid : 
+        
+    def test_invalid_type_list(self):
+        """Test with a correct latin name"""
+        with pytest.raises(ValueError):
+            valid_taxid([])
+        
+    def test_invalid_type_string(self):
+        """Test with string instead of a integer"""
+        with pytest.raises(ValueError):
+            valid_taxid("965")
+
+
+    def test_invalid_type_float(self):
+        """Test with float instead of a integer"""
+        with pytest.raises(ValueError):
+            valid_taxid(3.14)
+
+
+
+    def test_negative_taxid(self):
+        """Test with a negative integer"""
+        with pytest.raises(ValueError) :
+            valid_taxid(-5)    
+
+    def test_valid_taxid(self):
+        """Test with a correct taxid"""
+        result = valid_taxid(965)
+        assert result == 965
+
+    def test_invalid_taxid(self):
+        """Test with a non-existing taxid"""
+        result = valid_taxid(9999999999999)
+        assert result is None
+
+class TestConvertTaxid:
+    
+    def test_valid_int_taxid(self):
+        """Test with a valid integer taxid"""
+        result = convert_taxid(9606)
+        assert result == 9606
+    
+    def test_valid_string_taxid(self):
+        """Test with a valid string representation of taxid"""
+        result = convert_taxid("9606")
+        assert result == 9606
+    
+    def test_negative_taxid(self):
+        """Test with a negative integer"""
+        with pytest.raises(ValueError):
+            convert_taxid(-5)
+    
+    def test_invalid_string_float(self):
+        """Test with a float as string"""
+        with pytest.raises(ValueError):
+            convert_taxid("9.65")
+    
+    def test_invalid_type_float(self):
+        """Test with a float"""
+        with pytest.raises(ValueError):
+            convert_taxid(3.14)
+
+    def test_invalid_taxid(self):
+        """Test with a non-existing taxid"""
+        result = convert_taxid(9999999999999)
+        assert result is None
+
+
+
+
