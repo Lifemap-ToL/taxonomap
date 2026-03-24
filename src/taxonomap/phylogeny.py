3fd09b1cd05c4a8491b02ddaae61f8c9984edd50
ClaraCenta <clara.centa@gmail.com>
2026-03-24 13:59:15 +0100

    modified MRCA function for multiple taxids

--- src/taxonomap/phylogeny.py
+++ src/taxonomap/phylogeny.py
@@ -3,12 +3,14 @@ from taxonomap.solr_request import query_addi
 from taxonomap.utils.validation import convert_taxid
 
 
-def get_all_ascendant(value: int | str) -> list:
+
+
+def get_all_ascendant( value: int | str ) -> list:
 
     if type(value) is str:
         if value == "":
-            raise ValueError("Latin name cannot be empty")
-
+            raise ValueError(f"Latin name cannot be empty")
+        
         try:
             value = convert_taxid(value)
         except ValueError:
@@ -22,55 +24,60 @@ def get_all_ascendant(value: int | str) -> list:
     if value == 0:
         return []
 
-    docs = query_addi(fq=f"taxid:{value}", fl="ascend", rows=1)["response"]["docs"][0][
-        "ascend"
-    ]
+    docs = query_addi(fq=f"taxid:{value}", fl="ascend", rows=1)['response']["docs"][0]["ascend"]
 
+    
     if not docs:
         raise ValueError(f"No result found for taxid: {value}")
-
+    
     return docs
 
-    # def get_MRCA_taxid(*taxids:int) -> int : #en cours
+
+
+
+
+def get_MRCA_taxid(*taxids:int) -> int : #en cours
     """
     Finds the most recent common ancestor (MRCA) between two taxids.
-    Input: taxid1, taxid2
+    Input: 2 or more taxids
     Output: taxid number of MRCA of the given taxids.
     """
-    # to do : add verif for minimum 2 taxids
+    #to do : return sci names
+    
+    if len(taxids) < 2:
+        raise ValueError("Need at least 2 taxids to find MRCA")
 
-    # for taxid in taxids:
-    #     docs = query_addi(fq=f"taxid:{taxid}", fl="ascend", rows=1)
-    #     if not docs:
-    #         raise ValueError(f"Taxid {taxid} not found")
+    all_lineages = []
 
-    #     if len(docs1) == 0:
-    #         raise ValueError(f"No result found for taxid {taxid1}")
-    #     if len(docs2) == 0:
-    #         raise ValueError(f"No result found for taxid {taxid2}")
+    for taxid in taxids:
+        docs = query_addi(fq=f"taxid:{taxid}", fl="ascend")["response"]["docs"]
+        
+        if not docs:
+            raise ValueError(f"Taxid {taxid} not found")
 
-    #     # get ancestors list for each
-    #     lineage1 = docs1[0]["ascend"]
-    #     lineage2 = docs2[0]["ascend"]
+        lineage = docs[0]["ascend"] 
+        all_lineages.append(lineage)
+    
+        common_ancestors = set(all_lineages[0]) #here contains ancestors of first lineage
 
-    #     #transform in set to search it easily
-    #     common_ancestors = set(lineage1) & set(lineage2)
-    #     print(common_ancestors)
+        for lineage in all_lineages[1:]:
+            common_ancestors &= set(lineage) #intersection of first set with all the others
 
-    #     if len(common_ancestors) == 0:
-    #         raise ValueError(f"There are no common ancestor found for {taxid1} and {taxid2}")
+        for taxid in all_lineages[0]: # compare first lineage with common ancestors to find the first (common ancestors possibly in mixed order)
+            if taxid in common_ancestors:
+                return taxid
 
-    #     #loop from the end (more specific to less)
-    #     for taxid in lineage1:
-    #         if taxid in common_ancestors:
-    #             return taxid
+        
+        raise ValueError("could not determine MRCA!") #supposedly it should never happen        
 
-    #     raise ValueError("could not determine MRCA!") #supposedly it should never happen
+   
 
 
-# tests
+
+
+#tests
 if __name__ == "__main__":
     print(get_all_ascendant("965"))
-
-    # mrca = get_MRCA_taxid(965,989)
-    # print(f"MRCA of 965 and 989: {mrca}")
+    
+    print(f"MRCA of 965, 989 : {get_MRCA_taxid(965, 989)}")
+    print(f"MRCA of 9606, 9685, 10090: {get_MRCA_taxid(9606, 9685, 10090)}")
