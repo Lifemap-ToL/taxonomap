89d545956794f7741bfc067b5f8f7196e19773dc
ClaraCenta <clara.centa@gmail.com>
2026-03-24 09:58:00 +0100

    corrected add solr_request.py

---
+++ src/taxonomap/solr_request.py
@@ -0,0 +1,31 @@
+import requests
+from .config import SOLR_BASE_TAXO, SOLR_BASE_ADDI
+
+def query_solr(base_url, fq, fl, rows=1):    
+    try:
+        if isinstance(fq, str):
+            fq = [fq] #transforms argument fq in list for solr
+        
+        response = requests.post(
+            base_url,
+            data={"q": "*:*", "fq": fq, "fl": fl, "rows": rows},
+            timeout=10) 
+        
+        response.raise_for_status()
+        result = response.json()
+        
+        return result["response"]["docs"]
+    
+    
+    except requests.RequestException as e:
+        raise Exception(f"API error: {str(e)}")
+
+
+def query_taxo(fq, fl, rows=1):
+    """Query taxo db"""
+    return query_solr(SOLR_BASE_TAXO, fq, fl, rows)
+
+
+def query_addi(fq, fl, rows=1) :
+    """Query addi db"""
+    return query_solr(SOLR_BASE_ADDI, fq, fl, rows)
